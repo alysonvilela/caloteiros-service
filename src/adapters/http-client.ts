2e9ba281673e0d0ad5e4ac32ae6c18bd1e15f582
Alyson Vilela do Nascimento <alysonvn@MMB026004CPS.local>
2023-09-30 21:06:30 -0300

    feat: make http client singleton

--- src/adapters/http-client.ts
+++ src/adapters/http-client.ts
@@ -1,4 +1,16 @@
 import axios, { AxiosStatic } from "axios";
-export class HttpClient {
+class HttpClient {
   public request: AxiosStatic = axios;
+  private static instance: HttpClient;
+
+  private constructor() {}
+  static getInstance(): HttpClient {
+    if (!HttpClient.instance) {
+      HttpClient.instance = new HttpClient();
+    }
+
+    return HttpClient.instance;
+  }
 }
+
+export const httpClient = HttpClient.getInstance()
--- src/controllers/team/register-team-to-charge.ts
+++ src/controllers/team/register-team-to-charge.ts
@@ -1,11 +1,11 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { inMemoryRepositories } from "../../core/repositories/inmemory-impl";
 import { SendMessageToTeamsUseCase } from "../../core/usecases/send-message-to-team";
-import { HttpClient } from "../../adapters/http-client";
+import { httpClient } from "../../adapters/http-client";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const usecase = new SendMessageToTeamsUseCase(
-    new HttpClient(),
+    httpClient,
     inMemoryRepositories.serviceOwnerRepository,
     inMemoryRepositories.chargeRepository,
     inMemoryRepositories.teamRepository
