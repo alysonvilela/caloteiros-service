055dbf7cc9b143cbd60c252e1fbdc10afdabe0d5
Alyson Vilela do Nascimento <alysonvn@MMB026004CPS.local>
2023-10-06 15:18:22 -0300

    feat: add auth qr proxiies and current session

---
+++ src/controllers/charge/proxy-auth-qr.ts
@@ -0,0 +1,104 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import axios from "axios";
+
+const fetchQr = async () => {
+  try {
+    axios.defaults.headers.get["Access-Control-Allow-Origin"] = "*";
+    axios.defaults.headers.get["Referer-Control-Allow-Origin"] =
+      "http://localhost:3000/";
+    axios.defaults.headers.get["Host"] = "localhost:3000";
+    const url = "http://localhost:3000/api/default/auth/qr";
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+      headers: {
+        accept: "*/*",
+      },
+    });
+
+    const imageBuffer = Buffer.from(response.data, "binary").toString("base64");
+    return `data:${response.headers["content-type"]};base64,${imageBuffer}`;
+  } catch (err) {
+    console.log({err})
+
+    // if (err.message === "Can get QR code only in SCAN_QR_CODE status. The current status is 'STARTING'") {
+    //   return 'loading'
+    // }
+    return "/sessions/start";
+  }
+};
+
+const fetchStartSession = async () => {
+  const {data: existingSession} = await axios.get('http://localhost:3000/api/sessions/', {
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if(existingSession[0]?.status === 'WORKING') {
+    return
+  }
+
+  const url = "http://localhost:3000/api/sessions/start";
+
+  const requestData = {
+    name: "default",
+    config: {
+      proxy: null,
+      webhooks: [
+        {
+          url: "https://httpbin.org/post",
+          events: ["message"],
+          hmac: null,
+          retries: null,
+          customHeaders: null,
+        },
+      ],
+    },
+  };
+
+  await axios.post(url, requestData, {
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  await new Promise((res) =>
+    res(() => {
+      setTimeout(() => null, 1000);
+    })
+  );
+};
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  let base64String: string = "";
+
+  const qr = await fetchQr();
+
+  if (qr === "/sessions/start") {
+    await fetchStartSession();
+
+    const error = new Error(
+      "Your session has been started, please retry this same endpoint."
+    );
+    error.name = "Retry request";
+
+    return {
+      statusCode: 307,
+      body: JSON.stringify({
+        error: error.name,
+        message: error.message,
+      }),
+    };
+  }
+
+  base64String = qr;
+
+  return {
+    body: JSON.stringify({
+      image: base64String,
+    }),
+    statusCode: 200,
+  };
+};
